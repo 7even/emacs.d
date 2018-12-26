c16b538c3912d4125f226e7bf603ea213605c03f
Vsevolod Romashov <7@7vn.ru>
2018-12-26 16:40:38 +0300

    add kubernetes-mode

---
+++ elpa/kubernetes-0.12.0/kubernetes-pkg.el
@@ -0,0 +1,11 @@
+(define-package "kubernetes" "0.12.0" "Magit-like porcelain for Kubernetes."
+  '((emacs "25.1")
+    (dash "2.12.0")
+    (magit "2.8.0"))
+  :authors
+  '(("Chris Barrett" . "chris+emacs@walrus.cool"))
+  :maintainer
+  '("Chris Barrett" . "chris+emacs@walrus.cool"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
