22962f0c1d5da4cf9a0132aa2fad7cc1201bbfa6
Vsevolod Romashov <7@7vn.ru>
2020-05-10 19:15:24 +0300

    update projectile to 2.1.0

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -58,4 +58,6 @@
 (global-set-key (kbd "M-x") 'smex)
 
 ;; projectile everywhere!
-(projectile-global-mode)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
